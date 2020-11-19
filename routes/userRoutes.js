be74dde14a470203d3ca413e23c78a8ff17888e9
Cory Bass <cory.bass.07@gmail.com>
2020-11-19 12:46:42 -0600

    added route for getting user and registering user without hashing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,4 +6,28 @@ router.route("/").get((req, res) => {
   res.send("Hello from router");
 });
 
+router.route("/get-users").get((req, res) => {
+  UserModel.find((err, users) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json(users);
+    }
+  });
+});
+
+router.route("/register").post((req, res) => {
+  console.log(req.body);
+  const newUser = new UserModel(req.body);
+
+  newUser
+    .save()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(400).send("Unable to add user " + err);
+    });
+});
+
 module.exports = router;

bc0a2b4fb2f335cbe262558eeb0734c926542da0
Cory Bass <cory.bass.07@gmail.com>
2020-11-19 12:02:31 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const port = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.error("Connection Error", err);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
-app.listen(5000);
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
